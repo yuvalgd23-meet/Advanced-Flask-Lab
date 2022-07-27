ad1666482cd14eef8ba4fa372a740efc664cf328
yuvalgd23-meet <109908289+yuvalgd23-meet@users.noreply.github.com>
2022-07-27 18:28:04 +0300

    nice!

--- Advanced-Flask-Lab/app.py
+++ Advanced-Flask-Lab/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, jsonify, request, render_template, url_for
 import random
-import requests, json
+
 
 app = Flask(  # Create a flask app
     __name__,
@@ -9,8 +9,7 @@ app = Flask(  # Create a flask app
 )
 
 # Variables for tasks
-image_link = "https://scontent.ftlv21-1.fna.fbcdn.net/v/t31.18172-1/1502644_10152506946962507_2442851309927378964_o.png?stp=c49.0.148.148a_dst-png_p148x148&_nc_cat=111&ccb=1-7&_nc_sid=1eb0c7&_nc_ohc=d-R_wjIm2d8AX_NugPv&_nc_ht=scontent.ftlv21-1.fna&oh=00_AT8CSjiG8-biys_qQ9tU-yrxzJSp6HANxsz5c3mFVk-G2Q&oe=62FD0A1B"
-
+image_link = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTOEYRtVTAhnvbsvyzMQud7QSrI0I3Ap1GyIA&usqp=CAU"
 user_bio = "Middle East Entrepreneurs of Tomorrow. Enabling the next generation of Israeli and Palestinian leaders."
 
 posts = {
@@ -25,7 +24,8 @@ posts = {
 
 @app.route('/')  # '/' for the default page
 def home():
-    return render_template('index.html')
+    return render_template('index.html',image_link=image_link, user_bio=user_bio)
+
 
 
 @app.route('/about')  # '/' for the default page
